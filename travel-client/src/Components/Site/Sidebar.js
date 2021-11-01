92fda39d7321abc6bb45e5fac33c9f9ee0a932ca
Ginea Merrill <gineakanoe@gmail.com>
2021-11-01 15:45:27 -0400

    my bucketlist link on sidebar wasn't working

--- travel-client/src/Components/Site/Sidebar.js
+++ travel-client/src/Components/Site/Sidebar.js
@@ -5,7 +5,7 @@ import {
 } from 'react-router-dom';
 import Home from './Home';
 import Journals from '../Journals/Journals';
-import BucketList from '../Bucketlist/BucketList';
+import BucketList from '../BucketList/BucketList';
 
 const Sidebar = () => {
     return(
@@ -21,7 +21,7 @@ const Sidebar = () => {
                 <Switch>
                     <Route exact path='/home'><Home /></Route>
                     <Route exact path='/journals'><Journals /></Route>
-                    <Route exact path='/busketList'><BucketList /></Route>
+                    <Route exact path='/bucketList'><BucketList /></Route>
 
                     <Route exact path='/'><Home /></Route>
                 </Switch>
