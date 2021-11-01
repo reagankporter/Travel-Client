c5990c260fe717eab6ac331b2aa41f08ca2f01ca
Ginea Merrill <gineakanoe@gmail.com>
2021-11-01 15:30:53 -0400

    updated from group files

--- travel-client/src/App.js
+++ travel-client/src/App.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 import './App.css';
 import Auth from './Components/Auth/Auth'
-import Journal from './Components/Journals/Journals'
-
 import Footer from "./Components/Site/Footer";
 import Header from "./Components/Site/Header";
 import Sidebar from "./Components/Site/Sidebar";
+import Home from "./Components/Site/Home";
+
 import {
   BrowserRouter as Router
 } from 'react-router-dom';
@@ -24,26 +24,26 @@ function App() {
     setSessionToken(newToken);
   };
 
-  const clearLocalStorage = () =>{
-    localStorage.clear();
-    setSessionToken(undefined);
-  };
+  // const clearLocalStorage = () =>{
+  //   localStorage.clear();
+  //   setSessionToken(undefined);
+  // };
 
   const viewConductor = () => {
-    return sessionToken !== undefined ? <Journal sessionToken={sessionToken} /> : <Auth updateLocalStorage={updateLocalStorage} />;
+    return sessionToken !== undefined ? <Home sessionToken={sessionToken} /> : <Auth updateLocalStorage={updateLocalStorage} />;
   };
 
   return (
     <div className="App">
       <Header />
       <Router>
+      {viewConductor()}
         <Sidebar />
       </Router>
       <Footer />
-      {/* <h1>This is a test</h1>
-      {sessionToken} */}
+      <h1>This is a test</h1>
+      {sessionToken}
       {/* <Navbar clearLocalStorage={clearLocalStorage} /> */}
-      {viewConductor()}
     </div>
   );
 }
