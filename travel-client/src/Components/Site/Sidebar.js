fdd11eca8e84ec0584cdf5f5ecd61746b9c3bd59
Ginea Merrill <gineakanoe@gmail.com>
2021-11-02 11:44:00 -0400

    trying weird things with my bucket list

--- travel-client/src/Components/Site/Sidebar.js
+++ travel-client/src/Components/Site/Sidebar.js
@@ -3,7 +3,7 @@ import {
     Link,
     Switch
 } from 'react-router-dom';
-import Home from './Home';
+
 import Journals from '../Journals/Journals';
 import BucketList from '../BucketList/BucketList';
 
@@ -12,18 +12,18 @@ const Sidebar = () => {
         <div className='sidebar'>
             <div className='sidebar-list-styling'>
                 <ul className='sidebar-list list-unstyled'>
-                    <li><Link to='/'>Home</Link></li>
+ 
                     <li><Link to='/journals'>Travel Journal</Link></li>
                     <li><Link to='/bucketList'>Travel Bucket List</Link></li>
                 </ul>
             </div>
             <div className='sidebar-route'>
                 <Switch>
-                    <Route exact path='/home'><Home /></Route>
+
                     <Route exact path='/journals'><Journals /></Route>
                     <Route exact path='/bucketList'><BucketList /></Route>
 
-                    <Route exact path='/'><Home /></Route>
+
                 </Switch>
             </div>
         </div>
