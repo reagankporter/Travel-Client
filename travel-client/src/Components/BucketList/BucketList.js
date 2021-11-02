21a53a5d5311f5ad797b1f6d499cf1ddd0ce77a2
Ginea Merrill <gineakanoe@gmail.com>
2021-11-02 10:24:43 -0400

    moved get to main bucketlist page for testing, but it broke everything

--- travel-client/src/Components/BucketList/BucketList.js
+++ travel-client/src/Components/BucketList/BucketList.js
@@ -3,75 +3,123 @@ import './bucketList.css';
 import GetBucketList from './GetBucketList/GetBucketList';
 import DisplayBucketList from './DisplayBucketList/DisplayBucketList';
 import CreateBucketList from './CreateBucketList/CreateBucketList';
+import { CardGroup, Card, CardImg, CardBody, CardTitle, CardSubtitle, CardText, Button } from 'reactstrap';
 
 const BucketList = (props) => {
-    console.log(props);
-    const [bucketList, setBucketList] = useState([]);
-    const [createBucketList, setCreateBucketList] = useState(false);
 
+    const [bucketList, setBucketList] = useState([]);
 
     const fetchBucketList = () => {
-        let url = 'http://localhost:3000/bucketlist';
-
-        fetch(url, {
+        fetch('http://localhost:3000/bucketlist', {
             method: 'GET',
-            headers: new Headers({
+            headers: new Headers ({
                 'Content-Type': 'application/json',
-                'Authorization': props.sessionToken
+                'Authorization': props.token
             })
         })
-        .then(response => response.json())
-        .then(json => setBucketList(json))
-        .catch(err => console.log(err))
+        .then((res) => res.json())
+        .then((bucketlistData) => {
+            setBucketList(bucketlistData)
+            console.log(bucketlistData)
+        })
     }
-    console.log(bucketList);
+
     useEffect(() => {
         fetchBucketList();
-    }, [createBucketList])
-
-    const buttonHandler = () => {
-        setCreateBucketList(true)
-    }
+    }, [])
 
     return (
-        <div className='main'>
-            <div className='mainDiv'>
-
-                {createBucketList ? <CreateBucketList setCreateBucketList={setCreateBucketList} sessionToken={props.sessionToken} />
-                : null}
-                {!createBucketList ? <button onClick={buttonHandler}>Create Bucket List Item!</button>: null}
-
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Name of Place</th>
-                            <th>Location of Place</th>
-                            <th>Event of Interest</th>
-                            <th>This is a Bucket List Item Because:</th>
-                        </tr>
-                    </thead>
-            <tbody>
-                <DisplayBucketList bucketList={bucketList} />
-            </tbody>
-
-        </table>
-
-                <div className='card'>
-                    <img className='card-img' /> 
-                    <div className='card-body'>
-                        <DisplayBucketList bucketList={bucketList} />
-                        <h2 className='card-title'>nameOfPlace</h2>
-                        <h3 className='card-subtitle'>locationOfPlace</h3>
-                        <h3 className='card-subtitle'>eventOfInterest</h3>
-                        <p className='card-description'>whyAdded</p>
-                        <button className='placeLink'>Show Place</button>
-                    </div>
-                </div> 
-
-
-            </div>
+        <div>
+            <CardGroup>
+                <Card>
+                    <CardImg src='' alt='' />
+                    <CardBody>
+                        <CardTitle>
+                            Card title
+                        </CardTitle>
+                        <CardSubtitle>
+                            Card subtitle
+                        </CardSubtitle>
+                        <CardText>
+                            Card Text Goes Here
+                        </CardText>
+                        <Button>Edit</Button>   
+                        <Button>Delete</Button> 
+                    </CardBody>
+                </Card>
+            </CardGroup>
         </div>
     )
+
+
+
+    // console.log(props);
+    // const [bucketList, setBucketList] = useState([]);
+    // const [createBucketList, setCreateBucketList] = useState(false);
+
+
+    // const fetchBucketList = () => {
+    //     let url = 'http://localhost:3000/bucketlist';
+
+    //     fetch(url, {
+    //         method: 'GET',
+    //         headers: new Headers({
+    //             'Content-Type': 'application/json',
+    //             'Authorization': props.sessionToken
+    //         })
+    //     })
+    //     .then(response => response.json())
+    //     .then(json => setBucketList(json))
+    //     .catch(err => console.log(err))
+    // }
+    // console.log(bucketList);
+    // useEffect(() => {
+    //     fetchBucketList();
+    // }, [createBucketList])
+
+    // const buttonHandler = () => {
+    //     setCreateBucketList(true)
+    // }
+
+    // return (
+    //     <div className='main'>
+    //         <div className='mainDiv'>
+
+    //             {createBucketList ? <CreateBucketList setCreateBucketList={setCreateBucketList} sessionToken={props.sessionToken} />
+    //             : null}
+    //             {!createBucketList ? <button onClick={buttonHandler}>Create Bucket List Item!</button>: null}
+
+    //             <table>
+    //                 <thead>
+    //                     <tr>
+    //                         <th>Name of Place</th>
+    //                         <th>Location of Place</th>
+    //                         <th>Event of Interest</th>
+    //                         <th>This is a Bucket List Item Because:</th>
+    //                     </tr>
+    //                 </thead>
+    //         <tbody>
+    //             <DisplayBucketList bucketList={bucketList} />
+    //         </tbody>
+
+    //     </table>
+
+    //             <div className='card'>
+    //                 <img className='card-img' /> 
+    //                 <div className='card-body'>
+    //                     <DisplayBucketList bucketList={bucketList} />
+    //                     <h2 className='card-title'>nameOfPlace</h2>
+    //                     <h3 className='card-subtitle'>locationOfPlace</h3>
+    //                     <h3 className='card-subtitle'>eventOfInterest</h3>
+    //                     <p className='card-description'>whyAdded</p>
+    //                     <button className='placeLink'>Show Place</button>
+    //                 </div>
+    //             </div> 
+
+
+    //         </div>
+    //     </div>
+    // )
 
 };
 
