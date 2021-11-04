a7a522851a02270632bf8fbb912cfbdb6486fee1
Ginea Merrill <gineakanoe@gmail.com>
2021-11-03 20:22:16 -0400

    tried to get cards to be dynamic and failed

--- travel-client/src/Components/Site/Home.js
+++ travel-client/src/Components/Site/Home.js
@@ -1,35 +1,18 @@
-import React, {useState} from 'react';
-import {Navbar, Nav, NavItem, Button, Glyphicon, Modal, ModalHeader, ModalBody} from 'reactstrap';
+import React from 'react';
 import {Link} from 'react-router-dom';
 import Weatherapp from "../App/Weather";
-import Sidebarjs from "./Sidebar";
+import Sidebar from "./Sidebar";
 
 const Home = (props) => {
-    const [modalIsVisible, setModalIsVisible] = useState(false);
-
-    // const updateOn = () => {
-    //     setIsVisible(true);
-    // }
-
-    // const updateOff = () => {
-    //     setUpdateActive(false);
-    // }
-
-    const toggleModal = () => {
-        setModalIsVisible(!modalIsVisible)
-    }
 
     return(
         <>
-        <button onClick={toggleModal}><Glyphicon glyph='menu-hamburger'/></button>
-        <Modal isVisible={modalIsVisible}>   
             <div >
-                <Sidebarjs token={props.token} />
-                <h1>Travel Planning App</h1>
-                <hr />
+                {/* <h1>Travel Planning App</h1>
+                <hr /> */}
+                <Sidebar token={props.token} />
             </div>
         
-        </Modal>
         </>
     );
 };
